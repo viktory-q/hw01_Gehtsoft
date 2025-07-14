b0bc35ea98c46870f655c400cf899525a5bbfc6d
Viktoriya K <viktorydarki@gmail.com>
2025-07-14 18:54:06 +0300

    Output in 'Hello World' changed

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, World");
+        System.out.println("Hello from experiment branch!");
     }
 }
